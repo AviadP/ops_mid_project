8fad81a7026ec50da24cbe1e77d7174ecca3dfb0
Ubuntu <ubuntu@ip-172-31-86-223.ec2.internal>
2019-01-12 16:54:06 +0000

    updated config.yml

--- installation_scripts/install_kibana.sh
+++ installation_scripts/install_kibana.sh
@@ -4,4 +4,8 @@ wget https://artifacts.elastic.co/downloads/kibana/kibana-6.5.4-amd64.deb
 shasum -a 512 kibana-6.5.4-amd64.deb
 sudo dpkg -i kibana-6.5.4-amd64.deb
 
+IP_ADDR="$(ip -4 addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}')"
+sed -i 's|#server.host: "localhost"|server.host: '"$IP_ADDR"'|g' /etc/config/kibana.yml
+
+sudo systemctl stop kibana.service
 sudo systemctl start kibana.service
