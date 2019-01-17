be082953c263942f9f32822c7a235c3895e50d25
Aviad P <aviad.polak@gmail.com>
2019-01-17 15:35:54 +0200

    Create start_consul_srv.sh

---
+++ installation_scripts/start_consul_srv.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+docker run --name consul -d --net=host -v /consul/data:/consul/data \
+-e CONSUL_LOCAL_CONFIG='{
+"datacenter":"us-east-1",
+"server":true,
+"enable_debug":true
+}' consul:1.2.1 agent -server -bind 127.0.0.1 -bootstrap-expect=1 
+
+echo "Setup complete.
