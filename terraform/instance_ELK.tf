185b40a6749fd2eb53da586a7dd2976a4413c107
aviadP <aviad.polak@gmail.com>
2019-01-18 16:01:30 +0200

    updated and created

--- terraform/instance_ELK.tf
+++ terraform/instance_ELK.tf
@@ -1,5 +1,4 @@
-resource "aws_instance" "dummy_app" {
-    count = 2
+resource "aws_instance" "elk_stack" {
     ami = "${var.ami_id}"
     availability_zone = "us-east-1c"
     instance_type = "${var.instance_type}"
@@ -7,7 +6,8 @@ resource "aws_instance" "dummy_app" {
     subnet_id = "${aws_subnet.public_c.id}"
     vpc_security_group_ids = ["${aws_security_group.general_sg.id}"]
     associate_public_ip_address = true
-    user_data = "${file("../installation_scripts/install_docker.sh")}"
+    user_data = "${file("../installation_scripts/install_elasticsearch.sh")}"
+
 
     root_block_device {
       volume_type = "gp2"
@@ -20,7 +20,7 @@ resource "aws_instance" "dummy_app" {
     }
 
     tags {
-          "Name" = "Dummp_app-${count.index+1}"
+          "Name" = "ELK"
           "Project" = "Opsschool_mid_project"
     }
 }
--- terraform/instance_monitoring.tf
+++ terraform/instance_monitoring.tf
@@ -1,5 +1,4 @@
-resource "aws_instance" "dummy_app" {
-    count = 2
+resource "aws_instance" "monitoring" {
     ami = "${var.ami_id}"
     availability_zone = "us-east-1c"
     instance_type = "${var.instance_type}"
@@ -8,6 +7,7 @@ resource "aws_instance" "dummy_app" {
     vpc_security_group_ids = ["${aws_security_group.general_sg.id}"]
     associate_public_ip_address = true
     user_data = "${file("../installation_scripts/install_docker.sh")}"
+
 
     root_block_device {
       volume_type = "gp2"
@@ -20,7 +20,7 @@ resource "aws_instance" "dummy_app" {
     }
 
     tags {
-          "Name" = "Dummp_app-${count.index+1}"
+          "Name" = "monitoring"
           "Project" = "Opsschool_mid_project"
     }
 }
