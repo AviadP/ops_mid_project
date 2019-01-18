f06d76f8659579512501b578f6b40e8e591a11fe
aviadP <aviad.polak@gmail.com>
2019-01-18 22:09:59 +0200

    updated

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,15 +1,12 @@
 output "dummy_app" {
-    description = "dummp_app"
     value = "${aws_instance.dummy_app.*.public_ip}"
 }
 
 output "Grafana" {
-    description = "dummp_app"
     value = "${aws_instance.monitoring.public_ip}"
 }
 
 output "Kibana" {
-    description = "dummp_app"
     value = "${aws_instance.elk_stack.public_ip}"
 }
 
